6d876219d7346f887f5af6bc35d14c40e9a457f1
Romario <boriso-rom@yandex.ru>
2023-06-13 21:17:56 +0300

    added code to class toy

---
+++ Toy.java
@@ -0,0 +1,32 @@
+public class Toy {
+    private int id;
+    private String name;
+    private int frequency;
+
+    public Toy(int id2, String name2, int frequency2) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
